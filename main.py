9528e973a5c29e987def37f8ee1c2f073ae3685c
paxington181 <tsandrus@rogers.com>
2025-02-22 11:23:43 -0500

    60 FPS clock

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from constants import (
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    
+    dt = 0
 
     screen = pygame.display.set_mode(
         (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -24,8 +27,9 @@ def main():
 
 
 
-        pygame.display.flip()    
-
+        pygame.display.flip()
+     
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
