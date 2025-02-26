8d4f3ba06cb0d4cc102a6249c8e40b990042f2e3
paxington181 <tsandrus@rogers.com>
2025-02-26 13:17:21 -0500

    Weapon table start

--- weapontable.py
+++ weapontable.py
@@ -1,1 +1,6 @@
-#Weapon information to use in weapon Class constructor
+#Name = ("Display Name", "simple/martial", "damage die", "damage type", "property 1", "property 2", "property 3", "Range")
+#Melee and ranged broken up between tables
+
+weapon_club = ("Club", "Simple Melee", "1d4", "bludgeoning", "light", None, None, None)
+weapon_dagger = ("Dagger", "Simple Melee", "1d4", "piercing", "light", "finesse", "thrown", "20/60")
+weapon_greatclub = ("Greatclub", "Simple Melee", "1d8", "bludgeoning", "two-handed", None, None, None)
